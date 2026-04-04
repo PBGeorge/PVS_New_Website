623db1a038a59b3ddebddad2ca769c468184b283
PGeorge23 <120004890+PGeorge23@users.noreply.github.com>
2026-04-04 21:14:47 +0300

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -9,7 +9,7 @@ define('DB_USER', 'powervan_admin');  // e.g. myuser_pvs
 define('DB_PASS', 'w$.d^1K(#xAWLcVg');
 
 // Email address to receive submission notifications
-define('NOTIFY_EMAIL', 'pindaru.george94@gmail.com');
+define('NOTIFY_EMAIL', 'hello@powervantagesolutions.com');
 define('SITE_NAME',    'Power Vantage Solutions');
 
 // -----------------------------------------------
@@ -23,4 +23,4 @@ define('SMTP_PORT', 465);                              // 587 = TLS, 465 = SSL
 define('SMTP_USER', 'noreply@powervantagesolutions.com');
 define('SMTP_PASS', 'qju_TKV.fxz5fya.eph');
 define('SMTP_FROM', 'noreply@powervantagesolutions.com');
-define('SMTP_FROM_NAME', 'Power Vantage Solutions Website');
+define('SMTP_FROM_NAME', 'Power Vantage Solutions - NoReply');
